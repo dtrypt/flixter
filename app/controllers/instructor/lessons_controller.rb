a0712def4445f5fe2dd0d01c663f77392dc09911
Russell Kranz <russkranz@yahoo.com>
2016-11-29 10:17:56 +0000

    added permissions to lessons controller and refactored code

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,19 +1,31 @@
 class Instructor::LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_section
 
 def new
-  @section = Section.find(params[:section_id])
   @lesson = Lesson.new
 end
 
 def create
-  @section = Section.find(params[:section_id])
-  @lesson = @section.lessons.create(lesson_params) #.lessons is from model
-  redirect_to instructor_course_path(@section.course) #.course is from model (going up)
+  @lesson = current_section.lessons.create(lesson_params) #.lessons is from model
+  redirect_to instructor_course_path(current_section.course) #.course is from model (going up)
+end
 
 def show
 end
 
   private
+
+  def require_authorized_for_current_section
+    if current_section.course.user != current_user #section belongs to course, course belongs to user
+      return render text: 'Unauthorized', status: :unauthorized
+    end
+  end
+
+helper_method :current_section
+  def current_section
+    @current_section ||= Section.find(params[:section_id]) #stores id from DB
+  end
 
   def lesson_params
     params.require(:lesson).permit(:title, :subtitle)
