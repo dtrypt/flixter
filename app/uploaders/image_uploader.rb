2177f48cbd625026e7431855c5252c7eb6d90fa1
Russell Kranz <russkranz@yahoo.com>
2016-12-02 00:46:08 +0000

    reverted back to using fog instead of amazon s3 so I could continue for now...

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,8 +7,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   process resize_to_fill: [900, 300]
   # Choose what kind of storage to use for this uploader:
   #storage :file
-  #storage :fog
-  storage :aws
+  storage :fog
+  #storage :aws
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
