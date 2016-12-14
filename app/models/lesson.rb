85317165c934579fede449b3214722fff97f0c83
Russell Kranz <russkranz@yahoo.com>
2016-12-14 08:00:56 +0000

    37: added next & previous lessn bbuttons

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -5,4 +5,20 @@ class Lesson < ActiveRecord::Base
   include RankedModel
   ranks :row_order, :with_same => :section_id
 
+  def next_lesson
+    nextlesson = section.lessons.where("row_order > ?", self.row_order).rank(:row_order).first
+    if nextlesson.blank? && section.next_section
+      return section.next_section.lessons.rank(:row_order).first
+    end
+    return nextlesson
+  end
+
+  def previous_lesson
+    prevlesson = section.lessons.where("row_order < ?", self.row_order).rank(:row_order).last
+    if prevlesson.blank? && section.previous_section
+      return section.previous_section.lessons.rank(:row_order).last
+    end
+    return prevlesson
+  end
+
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -5,4 +5,14 @@ class Section < ActiveRecord::Base
   include RankedModel
   ranks :row_order, :with_same => :course_id
 
+  def next_section
+    nextsection = course.sections.where("row_order > ?", self.row_order).rank(:row_order).first
+    return nextsection
+  end
+
+  def previous_section
+    prevsection = course.sections.where("row_order < ?", self.row_order).rank(:row_order).last
+    return prevsection
+end
+
 end
